5b52130c86f3aebdbbc7b3127a2380dbad5aad6b
manqiie <130627035+manqiie@users.noreply.github.com>
2025-04-23 15:47:53 +0800

    change to your  password...

--- db_config.py
+++ db_config.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def configure_db(app):
     # Database configuration
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:qieqie3915@localhost/cms'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:YourPassword@localhost/cms'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     # Initialize the app with the extension
