1b1c798fe6a395d7bb859d25318ed2b2676bc601
Harish <hariram2002@gmail.com>
2025-04-24 22:12:13 +0800

    Delete function is done for users

--- db_config.py
+++ db_config.py
@@ -6,11 +6,11 @@ db = SQLAlchemy()
 def configure_db(app):
     # Load DB credentials from environment variables
     user = os.getenv("DB_USER", "root")
-    password = os.getenv("DB_PASSWORD", "YourPassword")
+    password = os.getenv("DB_PASSWORD", "harish11")
     host = os.getenv("DB_HOST", "localhost")
     db_name = os.getenv("DB_NAME", "cms")
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://scrumlord:scrumlord@localhost/scrumlorddb' #Admin for ur db:password@localhost/db name 
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:harish11@localhost/cms' #Admin for ur db:password@localhost/db name 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
